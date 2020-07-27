d08b7681ba8e429f6166f88568609247bae8452e
Ian Andrade Moreira <ianmoreira80@gmail.com>
2020-07-27 00:18:00 -0300

    feature: add route for store drink

--- routes/routes.php
+++ routes/routes.php
@@ -1,9 +1,6 @@
 <?php
 
-use Framework\Request;
-use Framework\Router;
-
-// include __DIR__ . '/vendor/autoload.php';
+use App\Controllers\DrinkController;
 
 use Phroute\Phroute\RouteCollector;
 use Phroute\Phroute\Dispatcher;
@@ -11,12 +8,15 @@ use App\Controllers\UserController;
 
 $collector = new RouteCollector();
 $userController = new UserController();
+$drinkContoller = new DrinkController();
+
 
 $collector->get('api/public/users',  [$userController, 'index']);
 $collector->post('api/public/users',  [$userController, 'store']);
 $collector->get('api/public/users/{id}',  [$userController, 'show']);
 $collector->put('api/public/users/{id}',  [$userController, 'update']);
 $collector->delete('api/public/users/{id}',  [$userController, 'destroy']);
+$collector->post('api/public/users/{userId}/drink',  [$drinkContoller, 'customStore']);
 
 $dispatcher = new Dispatcher($collector->getData());
 $response = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
