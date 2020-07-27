5b2b8370491c89c84b2ea0fed5fc320d0ecb6622
Ian Andrade Moreira <ianmoreira80@gmail.com>
2020-07-26 23:34:33 -0300

    feature: add model Drink

---
+++ src/App/Models/Drink.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Framework\AbstractModel;
+
+class Drink extends AbstractModel
+{
+    protected $fillable = ['drink_ml', 'user_id'];
+    protected $logTimestamp = TRUE;
+    protected $table = 'drinks';
+
+    public static function getRules(int $id = null): array
+    {
+        $rules = [
+            'drink_ml' => 'numeric'
+        ];
+
+        if (is_null($id)) {
+            $rules['drink_ml'] .= '|required';
+        }
+
+        return $rules;
+    }
+}
