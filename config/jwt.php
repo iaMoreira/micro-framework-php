e7d013c7056b78acb25a247e8135f1e8a724b209
Ian Andrade Moreira <ianmoreira80@gmail.com>
2020-07-28 02:33:29 -0300

    feature: config jwt

---
+++ config/jwt.php
@@ -0,0 +1,11 @@
+<?php
+// show error reporting
+error_reporting(E_ALL);
+ 
+// variables used for jwt
+$key = "example_key";
+$iss = "http://example.org";
+$aud = "http://example.com";
+$iat = 1356999524;
+$nbf = 1357000000;
+?>
