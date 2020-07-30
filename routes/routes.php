cb814f4e03c9b7f4dae223c8b868e798de9906c5
Ian Andrade Moreira <ianmoreira80@gmail.com>
2020-07-29 21:31:32 -0300

    fix: adjusts in routes

--- routes/routes.php
+++ routes/routes.php
@@ -4,24 +4,26 @@ use Phroute\Phroute\RouteCollector;
 use Phroute\Phroute\Dispatcher;
 
 $router = new RouteCollector();
-
-$router->post('api/public/login',  ['App\Controllers\LoginController', 'login']);
-$router->post('api/public/users',  ['App\Controllers\UserController', 'store']);
-
 $router->filter('auth', ['App\Middleware\Authenticate', 'handle']);
 $router->filter('owner', ['App\Middleware\ResourceOwner', 'handle']);
 
-$router->group(['before' => 'auth'], function ($router) {
+$router->group(['prefix' => request()->base()], function($router){
+    $router->post('login',  ['App\Controllers\LoginController', 'login']);
+    $router->post('users',  ['App\Controllers\UserController', 'store']);
 
-    $router->get('api/public/drinks/ranking',  ['App\Controllers\DrinkController', 'rankingToday']);
-    $router->get('api/public/users',  ['App\Controllers\UserController', 'index']);
-    $router->get('api/public/users/{id}',  ['App\Controllers\UserController', 'show']);
-    $router->get('api/public/users/{userId}/drink',  ['App\Controllers\DrinkController', 'index']);    
-    
-    $router->group(['before' => 'owner'], function ($router) {
-        $router->put('api/public/users/{id}',  ['App\Controllers\UserController', 'update']);
-        $router->delete('api/public/users/{id}',  ['App\Controllers\UserController', 'destroy']);
-        $router->post('api/public/users/{userId}/drink',  ['App\Controllers\DrinkController', 'store']);    
+
+    $router->group(['before' => 'auth', 'prefix' => 'users'], function ($router) {
+
+        $router->get('drinks/ranking',  ['App\Controllers\DrinkController', 'rankingToday']);
+        $router->get('/',  ['App\Controllers\UserController', 'index']);
+        $router->get('/{id}',  ['App\Controllers\UserController', 'show']);
+        $router->get('/{userId}/drink',  ['App\Controllers\DrinkController', 'index']);    
+        
+        $router->group(['before' => 'owner'], function ($router) {
+            $router->put('/{id}',  ['App\Controllers\UserController', 'update']);
+            $router->delete('/{id}',  ['App\Controllers\UserController', 'destroy']);
+            $router->post('/{userId}/drink',  ['App\Controllers\DrinkController', 'store']);    
+        });
     });
 });
 
