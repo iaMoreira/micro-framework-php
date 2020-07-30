1ce9056bd1746e9b199b35a1c833d35caabc3609
Ian Andrade Moreira <ianmoreira80@gmail.com>
2020-07-30 02:04:14 -0300

    feature: config dependency injection

---
+++ bootstrap/app.php
@@ -0,0 +1,24 @@
+<?php
+
+
+error_reporting(E_ALL);
+ini_set('display_errors', true);
+
+try {
+    require __DIR__."/../vendor/autoload.php";
+
+    $conn = \Framework\Database::getInstance('database');
+    \Framework\QueryBuilder::setConnection($conn);
+    \Framework\AbstractRepository::setConnection($conn);
+
+    $builder = new DI\ContainerBuilder();
+    $builder->addDefinitions(__DIR__ . '/container.php');
+    $container = $builder->build();
+    $loginController = $container->get('\App\Controllers\LoginController');
+
+    include __DIR__."/../routes/routes.php";
+    
+} catch (\Exception $e) {
+    echo $e->getMessage();
+}
+
---
+++ bootstrap/container.php
@@ -0,0 +1,8 @@
+<?php
+
+
+return [
+
+    App\Services\Contracts\IAuthService::class => DI\create(App\Services\AuthService::class),
+
+];
