ca574a22128f1cc95967f787677e2292752db37f
Ian Andrade Moreira <ianmoreira80@gmail.com>
2020-07-29 23:47:46 -0300

    feature: implementation contract IUserService

--- src/App/Services/Contracts/IUserService.php
+++ src/App/Services/Contracts/IUserService.php
@@ -2,7 +2,12 @@
 
 namespace App\Services\Contracts;
 
+use Framework\AbstractModel;
 use Framework\IAbstractService;
 
 interface IUserService extends IAbstractService
-{}
+{
+    public function store(array $data): AbstractModel;
+
+    public function update(int $id, array $data): AbstractModel;
+}
