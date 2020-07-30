db46a1560fe2f1fa8d39669e3911a643c707cf31
Ian Andrade Moreira <ianmoreira80@gmail.com>
2020-07-30 02:56:32 -0300

    feature: add provider RouterResolverProvider

---
+++ src/App/Providers/RouterResolverProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use DI\Container;
+use Phroute\Phroute\HandlerResolverInterface;
+
+class RouterResolverProvider implements HandlerResolverInterface
+{
+    private $container;
+
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    public function resolve($handler)
+    {
+        /*
+         * Only attempt resolve uninstantiated objects which will be in the form:
+         *
+         *      $handler = ['App\Controllers\Home', 'method'];
+         */
+        if(is_array($handler) and is_string($handler[0]))
+        {
+            $handler[0] = $this->container->get($handler[0]);
+        }
+
+        return $handler;
+    }
+}
